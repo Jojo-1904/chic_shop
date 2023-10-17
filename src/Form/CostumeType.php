8beb34f64aec09bc6bf9bea08424ff4fe0c1d105
Johnny <menabemenakely@yahoo.fr>
2023-10-17 20:26:06 +0200

    global  commit

--- src/Form/CostumeType.php
+++ src/Form/CostumeType.php
@@ -18,24 +18,25 @@ class CostumeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('isActive', CheckboxType::class, ['required'=>false, 'label'=>'Active', 'attr'=>
-            ["class"=>"form-check-input"], 'row_attr'=>["class"=>"form-switch mb-2"]] )
-            ->add('titre', TextType::class, ['required'=>true])
+            ->add('isActive', CheckboxType::class, ['required' => false, 'label' => 'Active', 'attr' =>
+            ["class" => "form-check-input"], 'row_attr' => ["class" => "form-switch mb-2"]])
+            ->add('titre', TextType::class, ['required' => true])
             ->add('description')
-            ->add('prix', MoneyType::class, ['required'=>true])
+            ->add('prix', MoneyType::class, ['required' => true])
             ->remove('slug')
-            ->add('categorie', EntityType::class, ['class'=>Categorie::class, 'choice_label'=>'nom', 'required'=>true])
+            ->add('categorie', EntityType::class, ['class' => Categorie::class, 'choice_label' => 'nom', 'required' => true])
             //https://symfony.com/doc/current/form//form_collections.html
-            ->add('images', CollectionType::class, ['entry_type'=>ImageType::class,
-            "allow_add"=>true, "by_reference"=>false, "allow_delete"=>true, 'label'=>false, 'entry_options'=>['fromBook'=>true, 'isNew'=>$options['isNew']]])
-        ;
+            ->add('images', CollectionType::class, [
+                'entry_type' => ImageType::class,
+                "allow_add" => true, "by_reference" => false, "allow_delete" => true, 'label' => false, 'entry_options' => ['fromBook' => true, 'isNew' => $options['isNew']]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Costume::class,
-            'isNew'=>true,
+            'isNew' => true,
         ]);
     }
 }
